06171f0b6072d472115ea5296844ddd2e3101776
Bosko4 <79284921+Bosko4@users.noreply.github.com>
2021-04-04 12:40:37 -0400

    update with revisions

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ datapath=os.path.join('.','Resources','budget_data.csv')
 with open(datapath) as datafile:
     data = csv.reader(datafile, delimiter=',')
     # skip headers row 
-    header = next(data)
+    headers = next(data)
+    firstmonth = next(data)
 
     # establish variables
     pvalue = int(0)
-    count = int(0)
-    total = int(0)
-    totalchange = int(0)
+    count = int(1)
+    total = int(firstmonth[1])
     aveprofit = int(0)
     avechange = int(0)
     hmonth = ()
     maxprofit = int(0)
     lmonth = ()
     maxloss = int(0)
-   
+    totalchange = -int(firstmonth[1])
 
     
 #step thru lines to create additional desired fields
@@ -31,40 +31,42 @@ with open(datapath) as datafile:
         row.append(int(row[1])-pvalue)      #change value, index[3]
         pvalue=int(row[1])
         count=count+1
-      
-             
+        #print(row)
+                      
         total += int(row[1])                #This works
         aveprofit = int(total / count)      #This works
-        totalchange += int(row[3])          #>>>>>>>>>>>THIS DOES NOT WORK (Grrrrr!)
-        avechange = int(totalchange/ count)
-
+        totalchange += int(row[3])          #This now works (once you strip 1st data line)
+        avechange = int(totalchange/ (count-1))
+        #print(totalchange) 
 
         if int(row[3]) > int(maxprofit):    #This works
             hmonth = row[0]
             maxprofit = int(row[3])
         
-            
     
         if int(row[3]) < int(maxloss):      #This works
             lmonth = row[0]
             maxloss = int(row[3])
-        
-            
     
+    #print(count)
+    #print(total,aveprofit)
+    #print(totalchange, avechange)
+    #print(lmonth, maxloss)
+
     
-    #print findings to terminal
+    #----print findings to terminal
     print(f"The total profit over the aggregate period of {count} months was {total}, with an average profit of {aveprofit} and average change of {avechange}.")  
     print(f"The greatest increase in profits occured in {hmonth} ({maxprofit}).")
-    print(f"The greatest decrease in profits occured in {lmonth} ({maxloss}.)")
+    print(f"The greatest decrease in profits occured in {lmonth} ({maxloss}).")
  
-#create path to output text file with summary analysis
+#---create path to output text file with summary analysis
 
 output_path = os.path.join('.','Analysis','Findings.txt')
 
-#write to output summary text file
+#---write to output summary text file
 with open(output_path, 'w', newline='') as txtfile:
 
-    txtfile.write(f"The total profit over the aggregate period of {count} months was {total}, with an average profit of {aveprofit}and average change of {avechange}.")
-    txtfile.write(f"The greatest increase in profits occured in {hmonth} ({maxprofit}).")
-    txtfile.write(f"The greatest decrease in profits occured in {lmonth} ({maxloss}.)")
+    txtfile.write(f"The total profit over the aggregate period of {count} months was {total}, with an average profit of {aveprofit}and average change of {avechange}."+'\n')
+    txtfile.write(f"The greatest increase in profits occured in {hmonth} ({maxprofit})."+'\n')
+    txtfile.write(f"The greatest decrease in profits occured in {lmonth} ({maxloss}.)"+'\n')
     txtfile.close
